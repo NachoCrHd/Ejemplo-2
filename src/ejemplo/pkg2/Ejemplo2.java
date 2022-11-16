fa67b0a1dc76687b5d521f4f04a7b375d7040fa5
invitado <invitado@LAB050210.itchii.lab>
2022-11-16 12:52:18 -0700

    ej 2

--- src/ejemplo/pkg2/Ejemplo2.java
+++ src/ejemplo/pkg2/Ejemplo2.java
@@ -4,7 +4,7 @@ public class Ejemplo2 {
 
     public static void main(String[] args) {
         System.out.println("Repositorio 2");
-        System.out.println("Ej 2");
+        System.out.println("Ejemplo función");
     }
     
 }
